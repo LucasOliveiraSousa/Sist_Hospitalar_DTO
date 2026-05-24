badc59c844b845b6e229862d2206021691db8c97
Lucas Sousa <89668332+LucasOliveiraSousa@users.noreply.github.com>
2026-05-24 15:11:28 -0300

    Add PacienteDTO

---
+++ src/main/java/com/hospitalar/dto/PacienteDTO.java
@@ -0,0 +1,30 @@
+package com.hospitalar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PacienteDTO {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private String endereco;
+    private String numeroResidencia;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private LocalDate dataCadastro;
+    private String status;
+
+}
